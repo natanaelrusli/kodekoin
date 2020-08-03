18e9a07ea3c7a64d000b6635eab999aeadf23034
Bagas <banabda@gmail.com>
2020-08-03 16:04:21 +0700

    update invoice model, controller, migration

---
+++ app/Http/Controllers/InvoiceController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Invoice;
+use Illuminate\Http\Request;
+
+class InvoiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(Invoice::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Invoice::create([
+            'id_invoice' => $request->get('id_invoice'),
+            'id_user' => $request->get('id_user'),
+            'email' => $request->get('email'),
+            'amount' => $request->get('amount'),
+            'status' => $request->get('status'),
+            'description' => $request->get('description'),
+            'invoice_url' => $request->get('invoice_url'),
+            'expiry_date' => $request->get('expiry_date')
+        ]);
+        return response()->json('Invoice Added Successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Invoice  $invoice
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Invoice $invoice)
+    {
+        return response()->json($invoice);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Invoice  $invoice
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Invoice $invoice)
+    {
+        return response()->json($invoice);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Invoice  $invoice
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Invoice $invoice)
+    {
+        Invoice::where('id', $invoice->id)
+            ->update($request->all());
+        return response()->json($request);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Invoice  $invoice
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Invoice $invoice)
+    {
+        $invoice->delete();
+        return response()->json('Invoice Deleted Successfully');
+    }
+}
