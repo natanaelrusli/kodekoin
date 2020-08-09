2abc0859e45b6c9ae61f9208fdcd862068903c74
natanaelrusli <64279959+natanaelrusli@users.noreply.github.com>
2020-08-09 15:27:06 +0700

    Fix dependencies

--- resources/js/containers/Login.js
+++ resources/js/containers/Login.js
@@ -8,7 +8,6 @@ import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
-import { ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
 import ReactDOM from "react-dom";
 import logo from "../images/logoimg.png";
 import "./css/Login.css";
