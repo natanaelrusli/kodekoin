517a67e3fb512e0fbc8658030e213d3683c97c97
natanaelrusli <64279959+natanaelrusli@users.noreply.github.com>
2020-08-24 01:32:29 +0700

    Update dashboard.js

--- resources/js/containers/Dashboard.js
+++ resources/js/containers/Dashboard.js
@@ -1,27 +1,43 @@
 import React, { useState, useEffect } from "react";
 import clsx from "clsx";
 import ReactDOM from "react-dom";
-import { makeStyles } from "@material-ui/core/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Drawer from "@material-ui/core/Drawer";
-import Box from "@material-ui/core/Box";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import List from "@material-ui/core/List";
-import Typography from "@material-ui/core/Typography";
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 import Button from "@material-ui/core/Button";
-import Divider from "@material-ui/core/Divider";
-import IconButton from "@material-ui/core/IconButton";
-import Container from "@material-ui/core/Container";
-import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import LockIcon from '@material-ui/icons/Lock';
+import PaymentIcon from '@material-ui/icons/Payment';
+
 import { css } from "@emotion/core";
 import PulseLoader from "react-spinners/PulseLoader";
+
 import Orders from "../components/Orders";
 import Profile from "../components/Profile";
 import ChangePassword from "../components/ChangePassword";
 import Navbar from "../components/TopNavbar";
+import ReferalForm from "../components/ReferalForm";
 import { updateInvoice } from "../components/DataFunctions";
+import { logoutHandler } from "../components/DataFunctions";
+import './css/navstyle.css';
+import '../components/css/listitems.css'
+
 function Copyright() {
     return (
         <Typography variant="body2" style={{ color: "white" }} align="center">
@@ -44,92 +60,94 @@ const drawerWidth = 230;
 
 const useStyles = makeStyles(theme => ({
     root: {
-        display: "flex",
-        flexDirection: "column",
-        height: "100%"
-    },
-    toolbar: {
-        paddingRight: 24 // keep right padding when drawer closed
-    },
-    toolbarIcon: {
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "flex-end",
-        padding: "0 8px",
-        ...theme.mixins.toolbar
-    },
-    appBar: {
-        zIndex: 1,
-        backgroundColor: "black",
-        transition: theme.transitions.create(["width", "margin"], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen
-        })
-    },
-    appBarShift: {
+        display: 'flex',
+        backgroundColor: '#2E2E2E',
+      },
+      toolbar: {
+        paddingRight: 24, // keep right padding when drawer closed
+        backgroundColor: '#2E2E2E',
+      },
+      toolbarIcon: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: '0 8px',
+        ...theme.mixins.toolbar,
+      },
+      appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(['width', 'margin'], {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.leavingScreen,
+        }),
+      },
+      appBarShift: {
         marginLeft: drawerWidth,
         width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(["width", "margin"], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen
-        })
-    },
-    menuButton: {
-        marginRight: 36
-    },
-    menuButtonHidden: {
-        display: "none"
-    },
-    title: {
-        flexGrow: 1
-    },
-    titleOrder: {
-        color: "#FF4646"
-    },
-    drawerPaper: {
-        position: "relative",
-        whiteSpace: "nowrap",
+        transition: theme.transitions.create(['width', 'margin'], {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+      },
+      menuButton: {
+        marginRight: 36,
+      },
+      menuButtonHidden: {
+        display: 'none',
+      },
+      title: {
+        marginLeft: '0px',
+        flexGrow: 1,
+      },
+      drawerPaper: {
+        backgroundColor: '#2E2E2E',
+        zIndex: '1',
+        position: 'relative',
+        whiteSpace: 'nowrap',
         width: drawerWidth,
-        transition: theme.transitions.create("width", {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen
-        })
-    },
-    drawerPaperClose: {
-        overflowX: "hidden",
-        transition: theme.transitions.create("width", {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen
+        transition: theme.transitions.create('width', {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+      },
+      drawerPaperClose: {
+        overflowX: 'hidden',
+        transition: theme.transitions.create('width', {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.leavingScreen,
         }),
         width: theme.spacing(7),
-        [theme.breakpoints.up("sm")]: {
-            width: theme.spacing(9)
-        }
-    },
-    content: {
+        [theme.breakpoints.up('sm')]: {
+          width: theme.spacing(9),
+        },
+      },
+      appBarSpacer: theme.mixins.toolbar,
+      content: {
         flexGrow: 1,
-        backgroundColor: "#222222"
-    },
-    container: {
+        height: '100vh',
+        overflow: 'auto',
+      },
+      container: {
         paddingTop: theme.spacing(4),
-        paddingBottom: theme.spacing(4)
-    },
-    paper: {
+        paddingBottom: theme.spacing(4),
+      },
+      paper: {
         padding: theme.spacing(2),
-        display: "flex",
-        overflow: "auto",
-        flexDirection: "column",
-        backgroundColor: "#f2f2f2 !important"
-    },
-    fixedHeight: {
-        height: 240
-    },
-    loadingAnimation: {
-        width: '100%',
         display: 'flex',
-        justifyContent: 'center',
-    }
-}));
+        overflow: 'auto',
+        flexDirection: 'column',
+      },
+      fixedHeight: {
+        height: 240,
+      },
+      navbutton: {
+          color: 'white',
+      },
+      loadingAnimation: {
+          display: 'flex',
+          justifyContent: 'center'
+      }
+    }));
 
 export default function Dashboard() {
     const classes = useStyles();
@@ -139,13 +157,17 @@ export default function Dashboard() {
     const userData = JSON.parse(localStorage.getItem("userData"));
     const invoices = JSON.parse(localStorage.getItem("invoices"));
     const [loading, setLoading] = useState(true);
-    // console.log(userData);
+    const [menuSelect, setMenuSelect] = useState('dashboard');
+
+    const [opensidebar, setOpensidebar] = useState(false);
+
     const handleDrawerOpen = () => {
-        setOpen(true);
+        setOpensidebar(true);
     };
     const handleDrawerClose = () => {
-        setOpen(false);
+        setOpensidebar(false);
     };
+    // console.log(userData);
     const chooseMenu = item => {
         setselectMenu(item);
     };
@@ -176,61 +198,139 @@ export default function Dashboard() {
 
     return (
         <div className={classes.root}>
-            <Navbar></Navbar>
+            <CssBaseline />
+            <AppBar position="fixed" className={clsx(classes.appBar, opensidebar && classes.appBarShift)}>
+                <Toolbar className={classes.toolbar}>
+                <IconButton
+                    edge="start"
+                    color="inherit"
+                    aria-label="open drawer"
+                    onClick={handleDrawerOpen}
+                    className={clsx(classes.menuButton, opensidebar && classes.menuButtonHidden)}
+                >
+                    <MenuIcon />
+                </IconButton>
+                    {users.map(user => (
+                        <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+                            Hi, {user.firstName}!
+                        </Typography>
+                    ))}
+                <Button href="/" className='navbutton'>
+                    Home
+                </Button>
+                <Button
+                    onClick={logoutHandler}
+                    className='navbutton'
+                    >
+                    Logout
+                </Button>
+                </Toolbar>
+            </AppBar>
+
+            <Drawer
+                variant="temporary"
+                classes={{
+                paper: clsx(classes.drawerPaper, !opensidebar && classes.drawerPaperClose),
+                }}
+                open={opensidebar}
+            >
+                <div className={classes.toolbarIcon}>
+                <IconButton onClick={handleDrawerClose}>
+                    <ChevronLeftIcon style={{ color : 'white' }}/>
+                </IconButton>
+                </div>
+                <Divider />
+                <List>
+                    <ListItem button onClick={()=>setMenuSelect('dashboard')}>
+                        <ListItemIcon>
+                            <DashboardIcon className='white'/>
+                        </ListItemIcon>
+                        <ListItemText primary="Dashboard" className='white' />
+                    </ListItem>
+                    {/* <ListItem button onClick={()=>setMenuSelect('orders')}>
+                        <ListItemIcon>
+                            <ShoppingCartIcon className='white' />
+                        </ListItemIcon>
+                        <ListItemText primary="Orders" className='white'/>
+                    </ListItem> */}
+                    <ListItem button onClick={()=>setMenuSelect('changepassword')}>
+                        <ListItemIcon>
+                            <LockIcon className='white'/>
+                        </ListItemIcon>
+                        <ListItemText primary="Change Password" className='white'/>
+                    </ListItem>
+                    <ListItem button onClick={()=>setMenuSelect('referal')}>
+                        <ListItemIcon>
+                            <PaymentIcon className='white'/>
+                        </ListItemIcon>
+                        <ListItemText primary="Referal Code" className='white' />
+                    </ListItem>
+                </List>
+            </Drawer>
 
             <main className={classes.content}>
                 <div className={classes.appBarSpacer} />
                 <Container maxWidth="lg" className={classes.container}>
                     <Grid container spacing={3}>
-                        {/* Recent Deposits */}
-                        <Grid item xs={12} md={12} lg={12}>
-                            <Paper className={classes.paper}>
-                                {users.map(user => (
-                                    <Profile
-                                        firstName={user.firstName}
-                                        lastName={user.lastName}
-                                        email={user.email}
-                                    />
-                                ))}
-                            </Paper>
-                        </Grid>
-
-                        {/* Change Password */}
-                        <Grid item xs={12}>
-                            <Paper className={classes.paper}>
-                                <ChangePassword />
-                            </Paper>
-                        </Grid>
-
-                        {/* Recent Orders */}
-                        <Grid item xs={12}>
-                            <Paper className={classes.paper}>
-                                {loading ? (
-                                    <div>
-                                        <h3 className={classes.titleOrder}>
-                                            Order History
-                                        </h3>
-                                        <div className={classes.loadingAnimation}>
-                                            <PulseLoader
-                                                css={override}
-                                                size={10}
-                                                color={"#FF4646"}
-                                                loading={loading}
+                            {/* Profile */}
+                            {
+                                menuSelect == 'dashboard' &&
+                                <Grid item xs={12} md={12} lg={12}>
+                                    <Paper className={classes.paper}>
+                                        {users.map(user => (
+                                            <Profile
+                                                firstName={user.firstName}
+                                                lastName={user.lastName}
+                                                email={user.email}
                                             />
-                                        </div>
-                                    </div>
-                                ) : (
-                                    <Orders />
-                                )}
-                            </Paper>
-                        </Grid>
-                    </Grid>
+                                        ))}
+                                    </Paper>
 
-                    <Box pt={4}>
-                        <Copyright />
-                    </Box>
-                </Container>
-            </main>
+                                    <Paper className={classes.paper} style={{ marginTop: '20px' }}>
+                                        {loading ? (
+                                            <div>
+                                                <h3 className={classes.titleOrder}>
+                                                    Order History
+                                                </h3>
+                                                <div className={classes.loadingAnimation}>
+                                                    <PulseLoader
+                                                        css={override}
+                                                        size={10}
+                                                        color={"#FF4646"}
+                                                        loading={loading}
+                                                    />
+                                                </div>
+                                            </div>
+                                        ) : (
+                                            <Orders />
+                                        )}
+                                    </Paper>
+                                </Grid>
+                            }
+                            
+                            {/* Change Password */}
+                            {
+                                menuSelect == 'changepassword' &&
+                                <Grid item xs={12}>
+                                    <Paper className={classes.paper}>
+                                        <ChangePassword />
+                                    </Paper>
+                                </Grid>
+                            }
+
+
+                            {/* Referal Code */}
+                            {
+                                menuSelect == 'referal' &&
+                                <Grid item xs={12}>
+                                    <Paper className={classes.paper}>
+                                        <ReferalForm />
+                                    </Paper>
+                                </Grid>
+                            }
+                        </Grid>
+                    </Container>
+                </main>
         </div>
     );
 }
