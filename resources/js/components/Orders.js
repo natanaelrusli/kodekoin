307e22cfeb926fc6fcd47222b441109814b76baf
natanaelrusli <64279959+natanaelrusli@users.noreply.github.com>
2020-08-23 18:17:03 +0700

    Add signup validation and Add icon on order detail dialog

--- resources/js/components/Orders.js
+++ resources/js/components/Orders.js
@@ -45,10 +45,23 @@ const Orders = () => {
     const invoices = JSON.parse(localStorage.getItem("invoices"));
 
     const [open, setOpen] = useState(false);
+    const [status, setStatus] = useState('');
 
     const handleOpen = i => {
         setOpen(true);
-        console.log(invoices);
+
+        if (i.status == 'SETTLED') {
+            setStatus('SETTLED');
+        }
+        else if (i.status == 'PENDING') {
+            setStatus('PENDING');
+        }
+        else if (i.status == 'EXPIRED') {
+            setStatus('EXPIRED');
+        }
+        console.log(status);
+
+        // console.log(invoices);
         detailOrder(i);
     };
     const handleClose = () => {
@@ -60,18 +73,6 @@ const Orders = () => {
     const detailOrder = i => {
         let methodshow = ""
         // Set the date we're counting down to
-        let countDownDate = new Date(i.expiry_date).getTime();
-        let now = new Date().getTime();
-        let hours = 0
-        let minutes = 0
-        let seconds = 0
-        let days = 0
-        let countdowntime = i.expiry_date
-        let distance = 0;
-
-        var interval = setInterval(() => {
-            distance = countDownDate - now;
-        }, 1000);
 
         if (i.retail != null) {
             methodshow = i.retail
@@ -99,15 +100,24 @@ const Orders = () => {
             <Modal show={open} size="md" aria-labelledby="contained-modal-title-vcenter" centered>
                 <Modal.Header><strong>Order Detail</strong></Modal.Header>
                 <Modal.Body>
-                    <p className='dialogText mb-1'>
-                        {detailText}
-                    </p>
+                    <div className="row">
+                        <div className="col-sm-7">
+                            <p className='dialogText mb-1'>
+                                {detailText}
+                            </p>
+                        </div>
+                        <div className="col-sm-5">
+                            {status == 'SETTLED' && <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fillRule="evenodd" clipRule="evenodd" className="statusIcon green"><path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm7 7.457l-9.005 9.565-4.995-5.865.761-.649 4.271 5.016 8.24-8.752.728.685z"/></svg>}
+                            {status == 'PENDING' && <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="statusIcon yellow"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm5.848 12.459c.202.038.202.333.001.372-1.907.361-6.045 1.111-6.547 1.111-.719 0-1.301-.582-1.301-1.301 0-.512.77-5.447 1.125-7.445.034-.192.312-.181.343.014l.985 6.238 5.394 1.011z"/></svg>}
+                            {status == 'EXPIRED' && <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="statusIcon red"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1.31 7.526c-.099-.807.528-1.526 1.348-1.526.771 0 1.377.676 1.28 1.451l-.757 6.053c-.035.283-.276.496-.561.496s-.526-.213-.562-.496l-.748-5.978zm1.31 10.724c-.69 0-1.25-.56-1.25-1.25s.56-1.25 1.25-1.25 1.25.56 1.25 1.25-.56 1.25-1.25 1.25z"/></svg>}
+                        </div>
+                    </div>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button className="mx-1 my-1" variant={"danger"} onClick={handleClose}>Close</Button>
+                    <Button className="mx-1 my-1" variant={"outline-danger"} onClick={handleClose}>Close</Button>
                 </Modal.Footer>
             </Modal>
-            
+
             <h3 className={classes.title}>Order History</h3>
 
             <Table size="small">
